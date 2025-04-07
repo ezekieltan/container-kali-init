08596814ede1d93fad8eba8d9930ffbc160afad9
ezekieltan <39935586+ezekieltan@users.noreply.github.com>
2025-04-07 16:59:20 +0800

    Update container-kali-init.sh

--- container-kali-init.sh
+++ container-kali-init.sh
@@ -5,6 +5,124 @@ apt install -y which wget curl file git zip nano openssl tcpdump iproute2 net-to
 #apt install -y nikto peass gobuster sqlmap metasploit-framework chisel sqlmap
 
 
+
+install_deb() {
+  local tool_name=$1
+  local extension=$2
+  local target_directory=$3
+  local backup_directory=$4
+
+  # Install the package
+  dpkg -i ${target_directory}/${tool_name}.${extension}
+
+  # Backup the .deb file
+  mv -f ${target_directory}/${tool_name}.${extension} ${backup_directory}
+}
+
+install_archive() {
+  local tool_name=$1
+  local extension=$2
+  local target_directory=$3
+  local backup_directory=$4
+  local strip_archive=$5
+  local encryption_password=$6
+  
+  # Clean up any previous extracted version
+  rm -rf ${target_directory}/${tool_name}
+
+  # Create directory and extract the downloaded file
+  mkdir -p ${target_directory}/${tool_name}
+ 
+  if [[ ${url} =~ \.tar\.(gz|xz)$ ]]; then
+    if [[ -n "$strip_archive" && ("$strip_archive" == "true") ]]; then
+      tar --strip-components=1 -xf ${target_directory}/${tool_name}.${extension} -C ${target_directory}/${tool_name}
+    else
+      tar -xf ${target_directory}/${tool_name}.${extension} -C ${target_directory}/${tool_name}
+    fi
+  elif [[ ${url} =~ \.zip$ ]]; then
+    unzip ${target_directory}/${tool_name}.zip -d ${target_directory}/${tool_name}
+  else
+    echo "Unsupported file type"
+    return 1
+  fi
+
+  # Move the archive file to the backup directory
+  mv -f ${target_directory}/${tool_name}.${extension} ${backup_directory}
+
+  # Encrypt backup if specified
+  if [ -n "$encryption_password" ]; then
+    # Encrypt backup
+    gpg --batch --yes --passphrase "$encryption_password" -c ${backup_directory}/${tool_name}.${extension}
+    
+    # Remove the unencrypted file from backup directory
+    rm -rf ${backup_directory}/${tool_name}.${extension}
+  fi
+
+  return 0
+    
+}
+
+install() {
+  local tool_name=$1
+  local url=$2
+  local target_directory=$3
+  local backup_directory=$4
+  local strip_archive=$5
+  local encryption_password=$6
+
+  if [[ "$url" =~ \.tar\.gz$ ]]; then
+    extension="tar.gz"
+  elif [[ "$url" =~ \.zip$ ]]; then
+    extension="zip"
+  elif [[ "$url" =~ \.tar\.xz$ ]]; then
+    extension="tar.xz"
+  elif [[ "$url" =~ \.deb$ ]]; then
+    extension="deb"
+  elif [[ "$url" =~ \.sh$ ]]; then
+    extension="sh"
+  else    
+    echo "Unsupported file type"
+    return 1
+  fi
+
+  # Download the archive/package/file
+  wget ${url} -P ${target_directory} -O ${tool_name}.${extension}
+
+
+
+  if [[ ${url} =~ \.(tar\.gz|tar\.xz|zip)$ ]]; then
+    install_archive $tool_name ${extension} $target_directory $backup_directory $strip_archive $encryption_password
+  elif [[ ${url} =~ \.deb$ ]]; then
+    install_deb ${tool_name} ${extension} $target_directory $backup_directory
+  elif [[ ${url} =~ \.sh$ ]]; then
+    chmod +x ${target_directory}/${tool_name}.sh
+  else
+    echo "Unsupported file type"
+    return 1
+  fi
+}
+
+# Function to download, extract, build, and encrypt a project
+garble_build() {
+  tool_name=$1  # specify the tool_name for extraction and other uses
+  target_directory=$2
+  random_seed_generated=$4
+
+  # Build the project
+  cd ${target_directory}/${tool_name}
+  if [ -n "$random_seed_generated" ]; then
+      garble --seed="$random_seed_generated" build -o ${target_directory}/${tool_name}.elf
+  else
+      garble build -o ${target_directory}/${tool_name}.elf
+  fi
+  cd ${target_directory}
+
+  # Remove the unencrypted source code directory
+  rm -rf ${target_directory}/${tool_name}
+}
+
+
+
 #INITAL SETUP
 target_directory="/root/software"
 mkdir -p ${target_directory}
@@ -38,29 +156,68 @@ echo "export PATH=\$PATH:/root/go/bin" >> /root/.bashrc
 source /root/.bashrc 
 
 
-chisel_version="1.10.1"
-wget https://github.com/jpillora/chisel/archive/refs/tags/v${chisel_version}.tar.gz -O chisel-v${chisel_version}.tar.gz
-tar -zxvf chisel-v${chisel_version}.tar.gz -C ${target_directory}
-cd ${target_directory}/chisel-${chisel_version}
-#garble build -o ${target_directory}/chisel.elf
-garble --seed="$random_seed_generated" build -o ${target_directory}/chisel.elf 
-cd ${target_directory}
-rm -rf chisel-${chisel_version}
-mv chisel-v${chisel_version}.tar.gz ${backup_directory}
-gpg --batch --yes --passphrase "$encryption_password" -c ${backup_directory}/chisel-v${chisel_version}.tar.gz
-rm -rf ${backup_directory}/chisel-v${chisel_version}.tar.gz
 
+# Install and backup Trivy
+trivy_version="0.60.0"
+trivy_url="https://github.com/aquasecurity/trivy/releases/download/v${trivy_version}/trivy_${trivy_version}_Linux-64bit.deb"
+install "trivy" $trivy_url $target_directory $backup_directory
+
+# Install and backup Grype
+grype_version="0.90.0"
+grype_url="https://github.com/anchore/grype/releases/download/v${grype_version}/grype_${grype_version}_linux_amd64.deb"
+install "grype" $grype_url $target_directory $backup_directory
+
+# Install and backup kubebench
+kubebench_version="0.10.4"
+kubebench_url="https://github.com/aquasecurity/kube-bench/releases/download/v${kubebench_version}/kube-bench_${kubebench_version}_linux_amd64.deb"
+install "kubebench" $kubebench_url $target_directory $backup_directory
+
+# Install and backup kubescape
+kubescape_version="3.0.31"
+kubescape_url="https://github.com/kubescape/kubescape/releases/download/v${kubescape_version}/kubescape-ubuntu-latest.tar.gz"
+install "kubescape" $kubescape_url $target_directory $backup_directory
+
+# Install and backup polaris
+polaris_version="9.6.2"
+polaris_url="https://github.com/FairwindsOps/polaris/releases/download/${polaris_version}/polaris_linux_amd64.tar.gz"
+install "polaris" $polaris_url $target_directory $backup_directory
+
+# Install and backup peirates
+peirates_version="1.1.25"
+peirates_url="https://github.com/inguardians/peirates/releases/download/v${peirates_version}/peirates-linux-amd64.tar.xz"
+install "peirates" $peirates_url $target_directory $backup_directory
+
+# Install and backup Kubiscan
+kubiscan_version="1.6"
+kubiscan_url="https://github.com/cyberark/KubiScan/archive/refs/tags/v${kubiscan_version}.tar.gz"
+install "KubiScan" $kubiscan_url $target_directory $backup_directory true
+python3 -m venv ${target_directory}/KubiScan/venv
+${target_directory}/KubiScan/venv/bin/pip install -r ${target_directory}/KubiScan/requirements.txt
+alias kubiscan='~/software/KubiScan/venv/bin/python ~/software/KubiScan/KubiScan.py'
+
+# Install and backup+encrypt chisel
+chisel_version="1.10.1"
+chisel_url="https://github.com/jpillora/chisel/archive/refs/tags/v${chisel_version}.tar.gz"
+install "chisel" $chisel_url $target_directory $backup_directory true $encryption_password
+garble_build "chisel" $target_directory $random_seed_generated
+
+# Install and backup+encrypt pspy
 pspy_version="1.2.1"
-wget https://github.com/DominicBreuker/pspy/archive/refs/tags/v${pspy_version}.tar.gz -O pspy-v${pspy_version}.tar.gz
-tar -zxvf pspy-v${pspy_version}.tar.gz -C ${target_directory}
-cd ${target_directory}/pspy-${pspy_version}
-#garble build -o ${target_directory}/pspy.elf
-garble --seed="$random_seed_generated" build -o ${target_directory}/pspy.elf
-cd ${target_directory}
-rm -rf pspy-${pspy_version}
-mv pspy-v${pspy_version}.tar.gz ${backup_directory}
-gpg --batch --yes --passphrase "$encryption_password" -c ${backup_directory}/pspy-v${pspy_version}.tar.gz
-rm -rf ${backup_directory}/pspy-v${pspy_version}.tar.gz
+pspy_url="https://github.com/DominicBreuker/pspy/archive/refs/tags/v${pspy_version}.tar.gz"
+install "pspy" $pspy_url $target_directory $backup_directory true $encryption_password
+garble_build "pspy" $target_directory $random_seed_generated
+
+# Install deepce
+deepce_url="https://github.com/stealthcopter/deepce/raw/main/deepce.sh"
+install "deepce" $deepce_url $target_directory $backup_directory
+
+# Get ExtensiveRoleCheck
+wget https://raw.githubusercontent.com/cyberark/kubernetes-rbac-audit/refs/heads/master/ExtensiveRoleCheck.py -P ${target_directory} -O ExtensiveRoleCheck.py
+
+# Get kubescore
+kubescore_version="1.19.0"
+wget https://github.com/zegl/kube-score/releases/download/v${kubescore_version}/kube-score_${kubescore_version}_linux_amd64 -P ${target_directory} -O kube-score_${kubescore_version}_linux_amd64.elf
+
 
 
 
@@ -77,58 +234,6 @@ rm -rf ${backup_directory}/pspy-v${pspy_version}.tar.gz
 
 
 
-
-
-
-
-
-
-trivy_version="0.60.0"
-wget https://github.com/aquasecurity/trivy/releases/download/v${trivy_version}/trivy_${trivy_version}_Linux-64bit.deb -P ${target_directory} -O trivy_${trivy_version}_Linux-64bit.deb
-dpkg -i ${target_directory}/trivy_${trivy_version}_Linux-64bit.deb
-mv -f ${target_directory}/trivy_${trivy_version}_Linux-64bit.deb ${backup_directory}
-
-grype_version="0.90.0"
-wget https://github.com/anchore/grype/releases/download/v0.90.0/grype_${grype_version}_linux_amd64.deb -P ${target_directory} -O grype_${grype_version}_linux_amd64.deb
-dpkg -i ${target_directory}/grype_${grype_version}_linux_amd64.deb
-mv -f ${target_directory}/grype_${grype_version}_linux_amd64.deb ${backup_directory}
-
-kubebench_version="0.10.4"
-wget https://github.com/aquasecurity/kube-bench/releases/download/v0.10.4/kube-bench_${kubebench_version}_linux_amd64.deb -P ${target_directory} -O kube-bench_${kubebench_version}_linux_amd64.deb
-dpkg -i ${target_directory}/kube-bench_${kubebench_version}_linux_amd64.deb
-mv -f ${target_directory}/kube-bench_${kubebench_version}_linux_amd64.deb ${backup_directory}
-
-wget https://github.com/stealthcopter/deepce/raw/main/deepce.sh -P ${target_directory}
-chmod +x ${target_directory}/deepce.sh
-
-wget https://raw.githubusercontent.com/cyberark/kubernetes-rbac-audit/refs/heads/master/ExtensiveRoleCheck.py -P ${target_directory} -O ExtensiveRoleCheck.py
-
-peirates_version="1.1.25"
-wget https://github.com/inguardians/peirates/releases/download/v${peirates_version}/peirates-linux-amd64.tar.xz -P ${target_directory} -O peirates-linux-amd64.tar.xz
-tar -xvf ${target_directory}/peirates-linux-amd64.tar.xz
-mv -f ${target_directory}/peirates-linux-amd64.tar.xz ${backup_directory}
-
-kubescore_version="1.19.0"
-wget https://github.com/zegl/kube-score/releases/download/v${kubescore_version}/kube-score_${kubescore_version}_linux_amd64 -P ${target_directory} -O kube-score_${kubescore_version}_linux_amd64.elf
-
-wget https://github.com/kubescape/kubescape/releases/download/v3.0.31/kubescape-ubuntu-latest.tar.gz -P ${target_directory} -O kubescape-ubuntu-latest.tar.gz
-rm -rf ${target_directory}/kubescape-ubuntu-latest
-mkdir -p ${target_directory}/kubescape-ubuntu-latest
-tar -zxvf ${target_directory}/kubescape-ubuntu-latest.tar.gz -C ${target_directory}/kubescape-ubuntu-latest
-mv -f ${target_directory}/kubescape-ubuntu-latest.tar.gz ${backup_directory}
-
-
-rm -rf ${target_directory}/KubiScan
-git clone https://github.com/cyberark/KubiScan.git ${target_directory}/KubiScan
-python3 -m venv ${target_directory}/KubiScan/venv
-${target_directory}/KubiScan/venv/bin/pip install -r ${target_directory}/KubiScan/requirements.txt
-alias kubiscan='~/software/KubiScan/venv/bin/python ~/software/KubiScan/KubiScan.py'
-
-wget https://github.com/FairwindsOps/polaris/releases/download/9.6.2/polaris_linux_amd64.tar.gz -P ${target_directory} -O polaris_linux_amd64.tar.gz
-rm -rf ${target_directory}/polaris_linux_amd64
-mkdir -p ${target_directory}/polaris_linux_amd64
-tar -zxvf ${target_directory}/polaris_linux_amd64.tar.gz -C ${target_directory}/polaris_linux_amd64
-mv -f ${target_directory}/polaris_linux_amd64.tar.gz ${backup_directory}
 
 #apt -y install kali-desktop-xfce
 #apt -y install xfce4 xfce4-goodies dbus-x11
